13db28b8f040787b5c82f961e6c3dd9d46ecc137
Eric_Lian <ExerciseBook@users.noreply.github.com>
2018-12-01 16:50:32 +0800

    初赛提交版本

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// JavaScript Document
+
+/*点赞功能*/
+var count = 124;
+function likeOver(obj) {
+    obj.innerHTML = "+" + count;
+}
+function likeOut(obj) {
+    obj.innerHTML = "Like it";
+}
+function likeClick(obj) {
+    if ( count == 124 ) {
+        count++;
+        alert("祝武大生日快乐！");
+        obj.innerHTML = "+" + count;
+    }
+    else alert("你已经点过赞了哦");
+}
+
+/*调整大小*/
+$(window).resize(function () {
+	"use strict";
+	$(".main-intro").css("height",$(window).height());
+	$(".main-full").css("height",$(window).height());
+});
+
+$(document).ready(function () {
+	"use strict";
+	var hideimg = 0;
+	$(".main-intro").css("height",$(window).height());
+	$(".main-full").css("height",$(window).height());
+	/*导航栏样式随滚动变换*/
+	$(document).scroll(function () {
+        if ($(document).scrollTop() > 0) {
+            $(".head-bg").addClass("menu-roll");
+        } else if ($(document).scrollTop() == 0) {
+            $(".head-bg").removeClass("menu-roll");
+        }
+	});
+	$("#btn1").click(function () {
+		$("body").animate({scrollTop: $("#1").offset().top - 60}, 1000);
+	});
+	$("#btn2").click(function () {
+		$("body").animate({scrollTop: $("#2").offset().top - 60}, 1000);
+	});
+	$("#btn3").click(function () {
+		$("body").animate({scrollTop: $("#3").offset().top - 60}, 1000);
+	});
+	$("#btn4").click(function () {
+		$("body").animate({scrollTop: $("#4").offset().top - 60}, 1000);
+	});
+	/*查看更多图片（功能实现中）*/
+	$('#viewmore').click(function(){
+		if (hideimg === 0){
+			$("hide-img").css("display","block");
+			$("#viewmore").text("隐藏图片");
+			hideimg = 1;
+		} else {
+            $("hide-img").css("display","none");
+            $("#viewmore").text("更多图片");
+            hideimg = 0;
+		}
+	})
+});
